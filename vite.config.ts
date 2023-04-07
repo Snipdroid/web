1149369e51f26758f5b01518e830241f6c29a8c5
Yanren <hi@imyan.ren>
2023-04-07 22:23:58 +0800

    fix: 修改 API 变量的获取方式

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path'
 import postCssPresetEnv from 'postcss-preset-env'
 import UnoCSS from 'unocss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
-const baseUrl = import.meta.env.VITE_API
+const baseUrl = loadEnv('', process.cwd()).VITE_API
 
 export default defineConfig({
   plugins: [solidPlugin(), UnoCSS()],
