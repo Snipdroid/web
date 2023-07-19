60818f53cf5ebf25d88790864c13d45d42aa02aa
Freesia <the_freesia@outlook.com>
2023-07-19 22:04:40 +0800

    chore: add unocss support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import UnoCSS from 'unocss/vite';
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -11,6 +12,7 @@ export default defineConfig({
     assetsDir: 'static',
   },
   plugins: [
+    UnoCSS(),
     react(),
     tsconfigPaths(),
     checker({
