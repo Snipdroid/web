17d6baddc34def1cc78d5b20c8639f174071ce7a
Butanediol <love@xnh.app>
2023-08-07 05:50:33 +0000

    Feat: expandable search & upload button

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, Search } from '@carbon/react';
+import { Button } from '@carbon/react';
+import { Upload } from '@carbon/icons-react';
+import ExpandableSearch from '@carbon/react/lib/components/ExpandableSearch/ExpandableSearch'
 import { debounce } from 'lodash';
 import { store } from '@/store';
 
@@ -19,24 +21,27 @@ export const Header: FC<HeaderProps> = () => {
   return (
     <div className="w-screen flex justify-center border-solid border-b border-[#E0E0E0]">
       <div className="h-12 box-border flex flex-justify-between items-center w-300">
-        <div className="h-8 w-45 flex flex-justify-between items-center mr-10">
+        <div className="h-8 w-45 flex flex-justify-between items-center">
           <div className="h-8 w-8">
             <img className="h-8 w-8" src="favicon.ico" alt="" />
           </div>
-          <div className="h-9 w-[123px] flex justify-center items-center text-7 font-400">
+          <div className="h-9 w-[123px] flex justify-center items-center text-7 font-400 ml-4 mr-4">
             {t('projectName')}
           </div>
         </div>
-        <div className="h12 flex-1 flex justify-center">
-          <Search
+        <div className="h12 flex-1 flex flex-justify-end">
+          <ExpandableSearch
             size="lg"
             id={'search_input'}
             placeholder="Name, package, activity"
             labelText={''}
             onChange={onSearchValueChange}
-          ></Search>
+          ></ExpandableSearch>
         </div>
-        <div className="ml-10 w-[195px] h12 flex">
+        <div className="w-[195px] h12 flex">
+          <Button hasIconOnly={true} kind='ghost'>
+            <Upload />
+          </Button>
           <Button className="w-20" kind="ghost">
             Sign up
           </Button>
